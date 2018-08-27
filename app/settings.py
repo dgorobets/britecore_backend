32d80f017152e919a6e34d030f16f10a47e62c80
Dmitry Gorobets <gorobets_da@soshace.com>
2018-08-28 01:31:03 +0500

    Adjust dj-static

--- app/settings.py
+++ app/settings.py
@@ -114,8 +114,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "..", "www", "static")
 
 # Logging
 LOGGING = {
